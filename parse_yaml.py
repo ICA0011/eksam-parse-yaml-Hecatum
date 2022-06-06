32490f81a5ca15065bcbe91187b8921b35a261f9
glpolj <glpolj@ttu.ee>
2022-06-06 13:56:19 +0300

    r

--- parse_yaml.py
+++ parse_yaml.py
@@ -5,17 +5,16 @@ url = 'http://upload.itcollege.ee/~aleksei/eksam.yaml'
 
 def parse_yaml(url):
     
-    ad = ''
+    ad = []
     req = urllib.request.urlopen(url)
     yaml_obj = yaml.safe_load(req)
 
     for user in yaml_obj:
         for key, val in user.items():
             if val['permission'] == 'admin':
-                ad = val['name']
+                ad.append(val['name'])
     
     return ad
-
 
 
 print(parse_yaml(url))
