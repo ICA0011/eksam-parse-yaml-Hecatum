ed2278515d18f077bf91f0863b7e714449954ead
glpolj <glpolj@ttu.ee>
2022-06-06 13:45:05 +0300

    1

--- parse_yaml.py
+++ parse_yaml.py
@@ -6,10 +6,9 @@ url = 'http://upload.itcollege.ee/~aleksei/eksam.yaml'
 def parse_yaml(url):
     
     req = urllib.request.urlopen(url)
+    yaml_obj = yaml.load(req)
     
     return req
 
 
-
-x = urllib.request.urlopen(url)
-print(x)
+print(parse_yaml(url))
